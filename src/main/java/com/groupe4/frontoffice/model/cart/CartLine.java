1325dd94c09654edf1b13caa2a339dbca4b396bc
Cyprien Chasseray <cyprien.chasseray@gmail.com>
2023-05-06 14:33:27 +0200

    Added relationship to product and carts

--- src/main/java/com/groupe4/frontoffice/model/cart/CartLine.java
+++ src/main/java/com/groupe4/frontoffice/model/cart/CartLine.java
@@ -1,5 +1,6 @@
 package com.groupe4.frontoffice.model.cart;
 
+import com.groupe4.frontoffice.model.product.Product;
 import com.groupe4.frontoffice.model.user.Adress;
 import jakarta.persistence.*;
 @Entity
@@ -7,13 +8,19 @@ public class CartLine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @ManyToOne
+    private Cart cart;
+    @ManyToOne
+    private Product product;
     private int quantity;
 
-    @ManyToOne
-    @JoinColumn(name = "id_cart")
-    private Cart cart;
-
     public CartLine() {
+    }
+
+    public CartLine(Cart cart, Product product, int quantity) {
+        this.cart = cart;
+        this.product = product;
+        this.quantity = quantity;
     }
 
     public int getId() {
@@ -22,5 +29,29 @@ public class CartLine {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
