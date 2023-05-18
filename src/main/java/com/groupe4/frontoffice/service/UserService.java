3b4bf7566199b042d291e1e014507144f85bf886
owantdelhiver <owenboutet@gmail.com>
2023-05-18 17:58:29 +0200

    add method findByEmail

--- src/main/java/com/groupe4/frontoffice/service/UserService.java
+++ src/main/java/com/groupe4/frontoffice/service/UserService.java
@@ -1,16 +1,10 @@
 package com.groupe4.frontoffice.service;
 
-import com.groupe4.frontoffice.model.product.Product;
 import com.groupe4.frontoffice.model.user.User;
-import com.groupe4.frontoffice.repository.product.ProductRepository;
 import com.groupe4.frontoffice.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class UserService {
@@ -20,4 +14,6 @@ public class UserService {
     public List<User> fetchAll(){
         return userRepository.findAll();
     }
+
+    public User findByEmail(String email) { return userRepository.findByEmail(email).get();}
 }
