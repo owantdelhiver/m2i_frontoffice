7b2cba8f9a865ef72189c0c537f6617da19556dd
owantdelhiver <owenboutet@gmail.com>
2023-05-18 17:58:58 +0200

    add Handler to set into HttpSession the email

--- src/main/java/com/groupe4/frontoffice/security/WebSecurityConfig.java
+++ src/main/java/com/groupe4/frontoffice/security/WebSecurityConfig.java
@@ -2,6 +2,9 @@ package com.groupe4.frontoffice.security;
 
 import com.groupe4.frontoffice.repository.user.UserRepository;
 import com.groupe4.frontoffice.service.UserDetailsServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +12,12 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
@@ -51,12 +54,22 @@ public class WebSecurityConfig {
         http.formLogin()
                 .loginPage("/login")
                 .usernameParameter("email")
-                .passwordParameter("password");
+                .passwordParameter("password")
+                .successHandler(new CustomAuthenticationSuccessHandler());
         http.logout()
                 .permitAll()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/login");
 
         return http.build();
+    }
+
+    private static class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+            HttpSession session = request.getSession();
+            String email = authentication.getName();
+            session.setAttribute("email", email);
+        }
     }
 }
