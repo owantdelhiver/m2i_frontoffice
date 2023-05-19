45cea1d5e46fb2dd1834a66dc1631643ea3dc5a1
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-19 19:22:08 +0200

    updated registration methods adding adress

--- src/main/java/com/groupe4/frontoffice/api/UserController.java
+++ src/main/java/com/groupe4/frontoffice/api/UserController.java
@@ -1,8 +1,11 @@
 package com.groupe4.frontoffice.api;
 
 import com.groupe4.frontoffice.dto.UserDto;
+import com.groupe4.frontoffice.model.user.Adress;
 import com.groupe4.frontoffice.model.user.User;
+import com.groupe4.frontoffice.service.AdressService;
 import com.groupe4.frontoffice.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
@@ -10,21 +13,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
-    @Autowired
+
     UserService userService;
-    @GetMapping("/registration")
-    public String registration(Model model){
-        UserDto user = new UserDto();
-        model.addAttribute("user", user);
+    AdressService adressService;
+    @GetMapping("/saveUser")
+    public String registration(Model model, User user, Adress adress){
+        model.addAttribute("user", new User());
+        model.addAttribute("adress", new Adress());
         return "registration";
     }
 
-    @PostMapping("/save")
-    public String saveUser(@ModelAttribute UserDto user, Model model){
-        model.addAttribute("user",user);
-        return"Contact";
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute User user,Adress adress, Model model){
+        userService.save(user);
+        adressService.save(adress);
+        return"registration";
     }
 }
