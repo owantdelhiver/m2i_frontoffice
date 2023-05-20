780c289982c13c82edbf3b795630e9fe3acc6405
owantdelhiver <owenboutet@gmail.com>
2023-05-20 18:22:05 +0200

    Change method controller to add to cart

--- src/main/java/com/groupe4/frontoffice/controller/CartLineController.java
+++ src/main/java/com/groupe4/frontoffice/controller/CartLineController.java
@@ -9,10 +9,9 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class CartLineController extends SuperController {
@@ -31,16 +30,10 @@ public class CartLineController extends SuperController {
     }
 
 
-    @PostMapping("/add-to-cart")
-    public String savetemToCartLine( @RequestParam(value = "id" , required = false, defaultValue = "1") int id,
-                                    @RequestParam(value = "quantity", defaultValue = "1") int quantity, Model model, HttpSession httpsession) {
-        Product product = productService.getById(id);
-        String email = (String) httpsession.getAttribute("email");
-        if (httpsession != null) {
-            CartLine cartLine = cartLineService.addItemToCart(product, quantity, email);
-            httpsession.setAttribute("totalItems", cartLineService.fetchAll());
-        }
-        return "redirect:/add-to-cart";
+        @PostMapping("/add-to-cart")
+    public String savetemToCartLine(@ModelAttribute CartLine cartLine,  HttpSession httpsession) {
+        User user = super.getUserSession(httpsession);
+        userService.addCartLine(user, cartLine);
+        return "redirect:/cart";
     }
-
 }
