204d774f8fe66db0ecff2a7ff8a800454b9a0945
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-20 14:48:54 +0200

    modified addItemtoCartLine

--- src/main/java/com/groupe4/frontoffice/controller/CartLineController.java
+++ src/main/java/com/groupe4/frontoffice/controller/CartLineController.java
@@ -5,7 +5,6 @@ import com.groupe4.frontoffice.model.product.Product;
 import com.groupe4.frontoffice.service.CartLineService;
 import com.groupe4.frontoffice.service.ProductService;
 import com.groupe4.frontoffice.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.Principal;
 import java.util.Optional;
 
 @Controller
