1b3e50cc5c6a4d751c5b1ed4b3b1f81c5217bbfd
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-20 15:55:57 +0200

    addItemToCartline method modified

--- src/main/java/com/groupe4/frontoffice/controller/CartLineController.java
+++ src/main/java/com/groupe4/frontoffice/controller/CartLineController.java
@@ -1,7 +1,7 @@
 package com.groupe4.frontoffice.controller;
-
 import com.groupe4.frontoffice.model.cart.CartLine;
 import com.groupe4.frontoffice.model.product.Product;
+import com.groupe4.frontoffice.model.user.User;
 import com.groupe4.frontoffice.service.CartLineService;
 import com.groupe4.frontoffice.service.ProductService;
 import com.groupe4.frontoffice.service.UserService;
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
 
 @Controller
 public class CartLineController extends SuperController {
@@ -25,12 +23,13 @@ public class CartLineController extends SuperController {
     UserService userService;
     @PostMapping("/add-to-cart")
     public String addItemToCartLine(@RequestParam("id") int id,
-                                @RequestParam(value = "quantity", defaultValue = "1") int quantity, Model model, HttpSession httpsession) {
+                                    @RequestParam(value = "quantity", defaultValue = "1") int quantity, Model model, HttpSession httpsession) {
 
-        Optional<Product> product = productService.getById(id);
+        Product product = productService.getById(id);
+        User user = super.getUserSession(httpsession);
+
         if (httpsession != null) {
-            String email = (String) httpsession.getAttribute("email");
-            CartLine cartLine = cartLineService.addItemToCart(product, quantity, email);
+            CartLine cartLine = cartLineService.addItemToCart(product, quantity, user.getCart());
             httpsession.setAttribute("totalItems", cartLineService.fetchAll());
             model.addAttribute("cartLine", cartLine);
         }
