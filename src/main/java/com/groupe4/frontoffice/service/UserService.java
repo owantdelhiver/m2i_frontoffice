3f5a3a027350bfe6d288e9cc16ccfeddc3ae8641
owantdelhiver <owenboutet@gmail.com>
2023-05-21 17:55:51 +0200

    check Optional user

--- src/main/java/com/groupe4/frontoffice/service/UserService.java
+++ src/main/java/com/groupe4/frontoffice/service/UserService.java
@@ -5,6 +5,7 @@ import com.groupe4.frontoffice.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserService {
@@ -17,5 +18,8 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User findByEmail(String email) { return userRepository.findByEmail(email).get();}
+    public User findByEmail(String email) {
+        Optional<User> user = userRepository.findByEmail(email);
+        return user.orElse(null);
+    }
 }
