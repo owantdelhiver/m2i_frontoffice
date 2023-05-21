5fcc478e88a58725104f08ed85699e9360c5d94d
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-21 20:17:06 +0200

    created ProductController

---
+++ src/main/java/com/groupe4/frontoffice/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.groupe4.frontoffice.controller;
+
+import com.groupe4.frontoffice.model.cart.CartLine;
+import com.groupe4.frontoffice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@Controller
+public class ProductController extends SuperController {
+    @Autowired
+    ProductService productService;
+
+        @GetMapping("product/{id}")
+        public String getProductbyId(@PathVariable int id, Model model, CartLine cartLine) {
+            model.addAttribute("product", productService.getById(id));
+            model.addAttribute("cartLine", cartLine);
+            return "product";
+    }
+
+
+}
