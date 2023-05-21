5b0086bc4164af113d7ca329ffba96992984890b
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-21 15:53:34 +0200

    removed lambok and added constructors getters and setters

--- src/main/java/com/groupe4/frontoffice/dto/UserDto.java
+++ src/main/java/com/groupe4/frontoffice/dto/UserDto.java
@@ -1,19 +1,10 @@
 package com.groupe4.frontoffice.dto;
-
+import com.groupe4.frontoffice.model.cart.Cart;
+import com.groupe4.frontoffice.model.order.Order;
 import com.groupe4.frontoffice.model.user.Adress;
 import com.groupe4.frontoffice.model.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+import java.util.List;
 
 public class UserDto extends User {
 
@@ -23,5 +14,66 @@ public class UserDto extends User {
     private String email;
     private String password;
 
+    public UserDto() {
+    }
 
+    public UserDto(String lastname, String firstname, String email, String password, Adress adress, Cart cart, List<Order> orders, int id, String lastname1, String firstname1, String email1, String password1) {
+        super(lastname, firstname, email, password, adress, cart, orders);
+        this.id = id;
+        this.lastname = lastname1;
+        this.firstname = firstname1;
+        this.email = email1;
+        this.password = password1;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String getFirstname() {
+        return firstname;
+    }
+
+    @Override
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
