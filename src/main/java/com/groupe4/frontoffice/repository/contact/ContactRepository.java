2328d7d9658eedbf8db205a2662e9c18a1c961b7
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-24 15:40:20 +0200

    created contact repository

--- src/main/java/com/groupe4/frontoffice/repository/contact/ContactRepository.java
+++ src/main/java/com/groupe4/frontoffice/repository/contact/ContactRepository.java
@@ -2,7 +2,9 @@ package com.groupe4.frontoffice.repository.contact;
 
 import com.groupe4.frontoffice.model.contact.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
 }
