9b6fca48da9597fc3af34a8cf2e69f61095af781
owantdelhiver <owenboutet@gmail.com>
2023-05-24 12:24:13 +0200

    delete import lombok + img on product page

--- src/main/java/com/groupe4/frontoffice/dto/UserDto.java
+++ src/main/java/com/groupe4/frontoffice/dto/UserDto.java
@@ -1,19 +1,8 @@
 package com.groupe4.frontoffice.dto;
 
-import com.groupe4.frontoffice.model.user.Adress;
+
 import com.groupe4.frontoffice.model.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 
 public class UserDto extends User {
 
