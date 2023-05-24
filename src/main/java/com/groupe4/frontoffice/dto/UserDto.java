2cc817dbd439f7062851523aa457f3dbe59669a4
owantdelhiver <owenboutet@gmail.com>
2023-05-24 11:57:33 +0200

    user dto

--- src/main/java/com/groupe4/frontoffice/dto/UserDto.java
+++ src/main/java/com/groupe4/frontoffice/dto/UserDto.java
@@ -13,7 +13,44 @@ public class UserDto extends User {
     private String lastname;
     private String firstname;
     private String email;
-    private String password;
 
+    @Override
+    public int getId() {
+        return id;
+    }
 
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String getFirstname() {
+        return firstname;
+    }
+
+    @Override
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
