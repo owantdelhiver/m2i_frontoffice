baa88c97e744869ae34c0eab6ce0f270d6140d65
Chahrazad13 <chahrazad.mrabet@outlook.fr>
2023-05-25 10:02:02 +0200

    removed unecessary info

--- src/main/java/com/groupe4/frontoffice/controller/TicketController.java
+++ src/main/java/com/groupe4/frontoffice/controller/TicketController.java
@@ -1,21 +1,22 @@
 package com.groupe4.frontoffice.controller;
 
-
 import com.groupe4.frontoffice.model.contact.Ticket;
 import com.groupe4.frontoffice.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TicketController {
 
     @Autowired
     TicketService ticketService;
+    @GetMapping(value = "/ticket")
 
-    @GetMapping("/ticket")
     public String registration(Model model, Ticket ticket){
         model.addAttribute("ticket", ticket);;
         return "ticket";
@@ -23,6 +24,10 @@ public class TicketController {
     @PostMapping("/ticket")
     public String userContact(Ticket ticket){
         ticketService.save(ticket);
-        return"redirect:/home";
+        return"redirect:/";
     }
+
+
+
+
 }
