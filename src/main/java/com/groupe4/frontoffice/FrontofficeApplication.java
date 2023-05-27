e28d7b5f68779a1e92e66135d3cf429c141c9e29
owantdelhiver <owenboutet@gmail.com>
2023-05-27 11:16:18 +0200

    new url for img

--- src/main/java/com/groupe4/frontoffice/FrontofficeApplication.java
+++ src/main/java/com/groupe4/frontoffice/FrontofficeApplication.java
@@ -1,25 +1,10 @@
 package com.groupe4.frontoffice;
 
-import com.groupe4.frontoffice.repository.cart.CartLineRepository;
-import com.groupe4.frontoffice.repository.product.ProductRepository;
-import com.groupe4.frontoffice.repository.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FrontofficeApplication  {
-
-    @Autowired
-    ProductRepository productRepository;
-
-    @Autowired
-    CartLineRepository cartLineRepository;
-
-    @Autowired
-    UserRepository userRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(FrontofficeApplication.class, args);
     }
