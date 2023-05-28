ab8fb5489f74462ecda4e1992726d4158719baeb
Cyprien Chasseray <cyprien.chasseray@gmail.com>
2023-05-28 14:51:46 +0200

    finalized front end

--- src/main/java/com/groupe4/frontoffice/controller/TicketController.java
+++ src/main/java/com/groupe4/frontoffice/controller/TicketController.java
@@ -1,15 +1,19 @@
 package com.groupe4.frontoffice.controller;
 
 import com.groupe4.frontoffice.model.contact.Ticket;
+import com.groupe4.frontoffice.model.user.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TicketController {
+public class TicketController extends SuperController {
 
     @GetMapping(value = "/ticket")
-    public String ticket(Model model, Ticket ticket){
+    public String ticket(Model model, Ticket ticket, HttpSession session){
+        User user = super.getUserSession(session);
+        model.addAttribute("user", user);;
         model.addAttribute("ticket", ticket);;
         return "ticket";
     }
